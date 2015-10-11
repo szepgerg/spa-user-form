a712af43baea1ead652877b018f1c4a3427eb759
gergo <szep.gergely@gravityrd.com>
2015-10-11 14:33:42 +0200

    some comments added

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -1,6 +1,7 @@
 var formApp = angular.module("formApp", ["ngMessages", "ui.bootstrap"]);
 
 formApp.controller("UserFormController", function ($scope, $http) {
+	//User data that will be submitted
 	$scope.data = {
 		userName: "",
 		email: "",
@@ -8,8 +9,10 @@ formApp.controller("UserFormController", function ($scope, $http) {
 		birthDate: null
 	};
 
+	//Indicates the state of the datepicker widget
 	$scope.datePickerOpened = false;
 
+	//List of available occupations
 	$scope.occupations = [
 		"Junior Software Developer",
 		"Senior Software Developer",
@@ -22,10 +25,12 @@ formApp.controller("UserFormController", function ($scope, $http) {
 
 	$scope.responseMsg = "";
 
+	//Sets the datepicker widget's state to opened
 	$scope.openDatePicker = function ($event) {
 		$scope.datePickerOpened = true;
 	};
 
+	//Submits the form, sends the user data to the server and resets the form
 	$scope.submitForm = function (isValid) {
 		if (isValid) {
 			$http.post("/api/userdata", $scope.formData)
@@ -39,6 +44,7 @@ formApp.controller("UserFormController", function ($scope, $http) {
 		}
 	};
 
+	//Resets the form
 	$scope.reset = function() {
 		$scope.formData = angular.copy($scope.data);
 		if ($scope.userForm) {
@@ -50,6 +56,7 @@ formApp.controller("UserFormController", function ($scope, $http) {
 	$scope.reset();
 });
 
+//Custom validation directive to determine if the user is older than 18
 formApp.directive("validateAge", function() {
 	return {
 		restrict: "A",
